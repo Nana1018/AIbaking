83adac29610d55335c548b112f01719aa278ffaf
Nana1018 <168632150+Nana1018@users.noreply.github.com>
2024-05-16 19:10:04 +0800

    Update testbakingflasklite_now.py

--- testbakingflasklite_now.py
+++ testbakingflasklite_now.py
@@ -314,5 +314,5 @@ def making():
 
 # return []
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5050)
